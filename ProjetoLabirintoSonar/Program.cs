5ccfa598e0e0a4714d21ce59de61c17d3ce1e475
felipetech4 <97922422+felipetech4@users.noreply.github.com>
2025-12-04 23:10:03 -0300

    declarando variaveis+definindo primeiros movimentos no tabuleiro

--- ProjetoLabirintoSonar/Program.cs
+++ ProjetoLabirintoSonar/Program.cs
@@ -1,2 +1,65 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int minimoGrid = 0;
+int maximoGrid = 9;
+int jogadorX;
+int jogadorY;
+int saidaX;
+int saidaY;
+int monstroX;
+int monstroY;
+string comando;
+bool comandoValido;
+
+Random sorteador = new Random();
+
+jogadorX = sorteador.Next(minimoGrid, maximoGrid);
+jogadorY = sorteador.Next(minimoGrid, maximoGrid);
+saidaX = sorteador.Next(minimoGrid, maximoGrid);
+saidaY = sorteador.Next(minimoGrid, maximoGrid);
+monstroX = sorteador.Next(minimoGrid, maximoGrid);
+monstroY = sorteador.Next(minimoGrid, maximoGrid);
+
+Console.WriteLine("Jogador X: " + jogadorX);
+Console.WriteLine("Jogador Y: " + jogadorY);
+
+Console.WriteLine("Informe um comando: W(Cima), A(Esquerda), S(Baixo), D (Direita)");
+
+do
+{
+    comando = Console.ReadLine();
+
+
+    switch (comando)
+    {
+        case "W":
+            comandoValido = true;
+            jogadorX--;
+            Console.WriteLine("Você clicou para Cima");
+            break;
+
+        case "A":
+            comandoValido = true;
+            jogadorY--;
+            Console.WriteLine("Você clicou para Esquerda");
+            break;
+
+        case "S":
+            comandoValido = true;
+            jogadorX++;
+            Console.WriteLine("Você clicou para Baixo");
+            break;
+
+        case "D":
+            comandoValido = true;
+            jogadorY++;
+            Console.WriteLine("Você clicou para Direita");
+            break;
+
+        default:
+            comandoValido = false;
+            Console.WriteLine("Informe um comando válido:");
+            break;
+    }
+} while (comandoValido == false);
+
+Console.WriteLine("Jogador X: " + jogadorX);
+Console.WriteLine("Jogador Y: " + jogadorY);
