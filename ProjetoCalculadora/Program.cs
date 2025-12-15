13eaafdf00c5bf20d0a6888759f0b97c0295d367
felipetech4 <97922422+felipetech4@users.noreply.github.com>
2025-12-14 22:10:21 -0300

    criando metodos das operacoes matematicas basicas

--- ProjetoCalculadora/Program.cs
+++ ProjetoCalculadora/Program.cs
@@ -1,2 +1,61 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Pendente receber dados de entrada sem precisar do Enter
+
+int n1;
+int n2;
+string operacao;
+double resultado = 0.0;
+
+Console.WriteLine("7 | 8 | 9");
+Console.WriteLine("4 | 5 | 6");
+Console.WriteLine("1 | 2 | 3");
+Console.WriteLine("0 | + | -");
+Console.WriteLine("* | / | =");
+Console.WriteLine("Cálculo: ");
+n1 = Convert.ToInt32(Console.ReadLine());
+operacao = Console.ReadLine();
+n2 = Convert.ToInt32(Console.ReadLine());
+
+switch (operacao)
+{
+    case "+":
+    resultado = adicao(n1, n2);
+    break;
+
+    case "-":
+    resultado = subtracao(n1, n2);
+    break;
+
+    case "*":
+    resultado = multiplicacao(n1, n2);
+    break;
+
+    case "/":
+    resultado = divisao(n1, n2);
+    break;
+
+    default:
+    Console.WriteLine("Operação inválida.");
+    break;
+}
+
+Console.WriteLine("Resultado: " + resultado);
+
+static float adicao (float n1, float n2)
+{
+    return n1 + n2;
+}
+
+static float subtracao (float n1, float n2)
+{
+    return n1 - n2;
+}
+
+static float multiplicacao (float n1, float n2)
+{
+    return n1 * n2;
+}
+
+static float divisao (float n1, float n2)
+{
+    return n1 / n2;
+}
--- ProjetoLabirintoSonar/obj/Debug/net8.0/ProjetoLabirintoSonar.AssemblyInfo.cs
+++ ProjetoLabirintoSonar/obj/Debug/net8.0/ProjetoLabirintoSonar.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProjetoLabirintoSonar")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d92d3c4b0060f27cf7e49252c2db521788009249")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4b0af92e4023df8997521df9ac9aabd2ff73d1a2")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProjetoLabirintoSonar")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProjetoLabirintoSonar")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
