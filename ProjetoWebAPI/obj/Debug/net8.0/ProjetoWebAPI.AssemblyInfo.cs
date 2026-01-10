4e9449de343919c8b8ed77a9018014569403a62b
felipetech4 <97922422+felipetech4@users.noreply.github.com>
2026-01-10 12:17:33 -0300

    adicionando suporte a controllers

--- ProjetoWebAPI/obj/Debug/net8.0/ProjetoWebAPI.AssemblyInfo.cs
+++ ProjetoWebAPI/obj/Debug/net8.0/ProjetoWebAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProjetoWebAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+36dd4f1f09baf82397aadb382bc249c20264c5bf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6ef143399e526dc5aa8cd7d0cc7cdb5b67451c1c")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProjetoWebAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProjetoWebAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
