ba8406c0a626817f90511bdd3ba8204cafef6a60
felipetech4 <97922422+felipetech4@users.noreply.github.com>
2026-01-10 13:30:31 -0300

    criei e modulei a classe modelo Heroi

---
+++ ProjetoCoracaoDigitalDaGuilda/Models/Heroi.cs
@@ -0,0 +1,12 @@
+public class Heroi
+{
+    public int Id { get; set; }
+    public string Nome { get ; set;}
+    public string Classe { get ; set;}
+    public int Nivel { get; set;}
+
+    public string Apresentar()
+    {
+        return $"Olá, sou {Nome}, um {Classe} nível {Nivel}!";
+    }
+}
