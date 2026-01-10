755ef73d14d1be2981dcf51546e36002bd8e57e0
felipetech4 <97922422+felipetech4@users.noreply.github.com>
2026-01-10 13:34:38 -0300

    iniciei a propriedade com um valor vazio para evitar erros de variável nula

--- ProjetoCoracaoDigitalDaGuilda/Models/Heroi.cs
+++ ProjetoCoracaoDigitalDaGuilda/Models/Heroi.cs
@@ -1,8 +1,8 @@
 public class Heroi
 {
     public int Id { get; set; }
-    public string Nome { get ; set;}
-    public string Classe { get ; set;}
+    public string Nome { get ; set;} = "";
+    public string Classe { get ; set;} = "";
     public int Nivel { get; set;}
 
     public string Apresentar()
